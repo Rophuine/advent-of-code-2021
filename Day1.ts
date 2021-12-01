5a2fcea13e3fc6743def6bec516a4f980a936522
Lionell Pack <lionellpack@gmail.com>
2021-12-01 16:25:33 +1000

    Tidied up some syntax.

--- Day1.ts
+++ Day1.ts
@@ -8,9 +8,9 @@ interface IArrayState {
 
 const nPointIncreaseCounter = (state: IArrayState, value: number) => {
     const previousSum = _.sum(state.previous);
-    const newArray = _(state.previous).slice(1).concat([value]).value();
+    const newArray = _.slice([...state.previous, value], 1);
     const newSum = _.sum(newArray);
-    return ({previous: newArray, increaseCount: newSum > previousSum ? state.increaseCount+1 : state.increaseCount});
+    return {previous: newArray, increaseCount: newSum > previousSum ? state.increaseCount+1 : state.increaseCount};
 }
 
 // Create a solver which selects n initial depths as the previous state, and passes the rest of the array to reduce to count the increases.
