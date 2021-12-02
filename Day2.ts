ec305bf66f595f11cddf0c555e1c7d77fd09353f
Lionell Pack <lionellpack@gmail.com>
2021-12-02 15:33:01 +1000

    Stage one

---
+++ Day2.ts
@@ -0,0 +1,27 @@
+import * as _ from 'lodash';
+import {readFileSync} from 'fs';
+
+interface movement {
+    direction: string;
+    distance: number;
+}
+
+interface position {
+    horizontal: number;
+    depth: number;
+}
+
+const filename = './Day2Sample.txt';
+const data: movement[] = readFileSync(filename, 'ascii').split(/\r\n/).map(s => s.split(' ')).map(array => ({direction: array[0], distance: parseInt(array[1])}));
+
+const moveFunction = (state: position, step: movement) => {
+    switch (step.direction) {
+        case 'forward': return {...state, horizontal: state.horizontal + step.distance};
+        case 'up': return {...state, depth: state.depth - step.distance};
+        case 'down': return {...state, depth: state.depth + step.distance};
+    }
+    throw new Error('Bad direction!');
+};
+
+let finalLocation = _.reduce(data, moveFunction, {horizontal: 0, depth: 0});
+console.log(finalLocation.depth * finalLocation.horizontal);
